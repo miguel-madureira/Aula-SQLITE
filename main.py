41e9207d4e9ce885744c2b7593315ddeb948e6de
Miguel <Migueloliveiraarruda@2gmail.com>
2025-09-26 09:57:44 -0300

    criar um banco de dados escola.db

---
+++ main.py
@@ -0,0 +1,8 @@
+#Criar um banco de dados SQLITE e uma tabela
+import sqlite3
+
+#Criar a conexão com o banco de dados chamado de "escola.db"
+conexao = sqlite3.connect("escola.db")
+
+#Criar o objetivo chamado de "cursor" que será usado para executar os comandos sql 
+cursor = conexao.cursor
