872c4506c5056328bec461d115675bb70bc9a2a1
Miguel <Migueloliveiraarruda@2gmail.com>
2025-09-26 16:46:10 -0300

    update função delete

--- main.py
+++ main.py
@@ -72,3 +72,34 @@ cursor = conexao.cursor()
 # print(f"Alunos dp curso de {pesquisar}")
 # for linha in cursor.fetchall():
 #     print(f"NOME: {linha[0]} | IDADE: {linha[1]}") 
+
+
+# #Deletar dados do banco.
+#cursor.execute("DELETE FROM alunos WHERE id = ?", (1,))
+# conexao.commit()
+# #Sempre fechar a conexão com banco de dados
+# conexao.close()
+
+def deletar_aluno(id_aluno):
+    try:
+        conexao = sqlite3.connect("escola.db")
+        cursor = conexao.cursor()
+
+        cursor.execute("DELETE FROM alunos WHERE id = ?", (id_aluno,))
+        #Confirmando as alterações no banco
+        conexao.commit()
+        # Verificando se algum aluno foi realmente deletado
+        if cursor.rowcount > 0:
+            print("Aluno foi removido com sucesso")
+        else:
+            print("Nenhum aluno encontrado com o ID fornecido.")
+    except Exception as erro:
+        #Caso ocorra algum erro no banco
+        print(f"erro ao tentar excluir aluno {erro}")
+    finally:
+        #Sempre fechar a conexão
+        if conexao:
+            conexao.close()
+
+deletar = int(input("Digite o id do aluno que deseja deletar: "))
+deletar_aluno(deletar)
